b974a9fa756bda5886e2526296018b7a2b841a63
Manon Jacquin <manon.jacquin@gmail.com>
2020-02-21 17:40:37 +0000

    Adds pagination to the query to limit big load of data

--- src/schema.js
+++ src/schema.js
@@ -1,53 +1,73 @@
-const { gql } = require('apollo-server');
+const { gql } = require("apollo-server");
 
 const typeDefs = gql`
-    type Launch {
-        id: ID!
-        site: String
-        mission: Mission
-        rocket: Rocket
-        isBooked: Boolean!
-    }
+  type Launch {
+    id: ID!
+    site: String
+    mission: Mission
+    rocket: Rocket
+    isBooked: Boolean!
+  }
 
-    type Rocket {
-        id: ID!
-        name: String
-        type: String
-    }
+  type Rocket {
+    id: ID!
+    name: String
+    type: String
+  }
 
-    type User {
-        id: ID!
-        email: String!
-        trips: [Launch]!
-    }
+  type User {
+    id: ID!
+    email: String!
+    trips: [Launch]!
+  }
 
-    type Mission {
-        name: String
-        missionPatch(size: PatchSize): String
-    }
+  type Mission {
+    name: String
+    missionPatch(size: PatchSize): String
+  }
 
-    enum PatchSize {
-        SMALL
-        LARGE
-    }
+  enum PatchSize {
+    SMALL
+    LARGE
+  }
 
-    type Query {
-        launches: [Launch]!
-        launch(id: ID!): Launch
-        me: User
-    }
+  type Query {
+    launches( # replace the current launches query with this one.
+      """
+      The number of results to show. Must be >= 1. Default = 20
+      """
+      pageSize: Int
+      """
+      If you add a cursor here, it will only return results _after_ this cursor
+      """
+      after: String
+    ): LaunchConnection!
+    launch(id: ID!): Launch
+    me: User
+  }
 
-    type Mutation {
-        bookTrips(launchIds: [ID]!): TripUpdateResponse!
-        cancelTrip(launchIds: [ID]!): TripUpdateResponse!
-        login(email: String): String 
-    }
+  """
+  Simple wrapper around our list of launches that contains a cursor to the
+  last item in the list. Pass this cursor to the launches query to fetch results
+  after these.
+  """
+  type LaunchConnection { # add this below the Query type as an additional type.
+    cursor: String!
+    hasMore: Boolean!
+    launches: [Launch]!
+  }
 
-    type TripUpdateResponse {
-        success: Boolean!
-        message: String
-        launches: [Launch]
-    }
-`
+  type Mutation {
+    bookTrips(launchIds: [ID]!): TripUpdateResponse!
+    cancelTrip(launchIds: [ID]!): TripUpdateResponse!
+    login(email: String): String
+  }
 
-module.exports = typeDefs
+  type TripUpdateResponse {
+    success: Boolean!
+    message: String
+    launches: [Launch]
+  }
+`;
+
+module.exports = typeDefs;
