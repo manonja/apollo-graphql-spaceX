19e29af380b07980358d14c5c9c279cedc2283ac
Manon Jacquin <manon.jacquin@gmail.com>
2020-02-21 17:06:46 +0000

    adds the store to index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,22 @@
 // imports the ApolloServer class from apollo-server, along with our (currently undefined) schema from src/schema.js.
 const { ApolloServer } = require('apollo-server');
 const typeDefs = require('./schema');
+const { createStore } = require('./utils');
 
+const LaunchAPI = require('./datasources/launch');
+const UserAPI = require('./datasources/user');
+
+const store = createStore();
 //  creates a new instance of ApolloServer and passes it the imported schema via the typeDefs property.
-const server = new ApolloServer({typeDefs});
+const server = new ApolloServer({
+    typeDefs,
+    dataSources: () => ({
+        launchAPI: new LaunchAPI(),
+        userAPI: new UserAPI({ store })
+    })
+});
+
+
+server.listen().then(({ url }) => {
+    console.log(`Server ready at ${url}`)
+});
